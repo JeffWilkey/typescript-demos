4ca4bcf1d7506c5c2c2747ebd427a353b3dc7603
Jeff Wilkey <jeff@manscaped.com>
2020-04-10 00:30:00 -0700

    02_variables_and_type_annotation

---
+++ annotations/variables.ts
@@ -0,0 +1,65 @@
+/*
+ @module variables
+ Typescript variables with explicit type annotations
+ Type annotation should be used:
+   - When a function returns the 'any' type and we need to clarify the value
+   - When we declare a variable on one line then initialize it later
+   - When we want a variable to have a type that can't be inferred
+
+ *NOTE*
+   None of the following type annotations are required because they are inferred
+ */
+
+// Primitives
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built-ins
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') foundWord = true;
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
